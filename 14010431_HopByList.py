2b481d8c61e6ead4dfeb8f06829ed24a5bd92898
adelmarzban <adelmarzban@gmail.com>
2022-07-22 01:18:41 +0430

    version 1

--- 14010431_HopByList.py
+++ 14010431_HopByList.py
@@ -1,5 +1,6 @@
 #### Hope Game by Applying LIST ######
-a = [*range(1, 101)]
+Adad = int(100)
+a = [*range(1, Adad)]
 for i in range(0, len(a)):
     if a[i] % 3 == 0 and a[i] % 5 == 0:
         a[i] = "Hope Hip"
